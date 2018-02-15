eaa60180e0bb19770d5a9e0a6f09732547a53714
Andrew <walker.aj325@gmail.com>
2018-02-15 07:23:33 -0500

    reset permissions, create lots of dirs, correct xattr name

--- not_samba/small_file_maker.sh
+++ not_samba/small_file_maker.sh
@@ -2,20 +2,37 @@
 
 # Script to create lots of small files with xattrs for samba testing
 
-target_num_files=100000
-target_dir="/tmp/foo"
+target_num_files=10
+target_num_dirs=10
+target_dir="/mnt/tank/smb_small_files2"
 min_count=1  #minimum value for blocks in dd
 max_count=5  #maximum value for blocks in dd
 dd_bs=16384  #16KB
-stream_name="DosStream.Afp_Resource"
-i=1
-mkdir $target_dir
+stream_name='DosStream.Afp_Resource:$DATA'
+owner="root"
+group="wheel"
 
-while [ $i -le $target_num_files ]
+
+i=1
+dir_iteration=1
+mkdir -p $target_dir
+
+while [ $dir_iteration -le $target_num_dirs ]
 do
-        rnd_file=$(mktemp "$target_dir"/tmp.XXXXXXXX)
-        dd_count=$(jot -r 1 $min_count $max_count)
-        dd if=/dev/random of=$rnd_file count=$dd_count bs=$dd_bs
-	dd if=/dev/random count=$dd_count bs=$dd_bs | setextattr -in "user" "$stream_name" $rnd_file
-        i=$(($i + 1))
+rnd_dir=$(mktemp -d "$target_dir"/smbtestdir.XXXXX)
+echo "$rnd_dir"
+        while [ $i -le $target_num_files ]
+        do
+            rnd_file=$(mktemp "$rnd_dir"/smbtest.XXXXXXXX)
+            dd_count=$(jot -r 1 $min_count $max_count)
+            dd if=/dev/random of=$rnd_file count=$dd_count bs=$dd_bs status=none
+            dd if=/dev/random count=$dd_count bs=$dd_bs status=none | \
+                setextattr -in "user" "$stream_name" $rnd_file 
+            i=$(($i + 1))
+        done
+dir_iteration=$(($dir_iteration + 1))
+i=1
 done
+
+echo "setting default permissions on $target_dir"
+winacl -a reset -O "$owner" -G "$group" -r -p "$target_dir"
