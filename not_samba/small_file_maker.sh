2a1729fa95d310168915377c7473ade84144b817
Andrew <walker.aj325@gmail.com>
2018-02-16 18:35:34 -0500

    remove extra lf at end of resulting dosattrib

--- not_samba/small_file_maker.sh
+++ not_samba/small_file_maker.sh
@@ -3,15 +3,15 @@
 # Script to create lots of small files with xattrs for samba testing
 # Depends on openssl and winacl
 
-target_num_files=10
-target_num_dirs=10
-target_dir="/mnt/dozer/smb_small_files2"
-min_count=1  #minimum value for blocks in dd
-max_count=5  #maximum value for blocks in dd
+target_num_files=1000
+target_num_dirs=100
+target_dir="/mnt/flashyflashflash/SSD_SHARE/smb_small_files2"
+min_count=200  #minimum value for blocks in dd
+max_count=700  #maximum value for blocks in dd
 dd_bs=16384  #16KB
 stream_name='DosStream.Afp_Resource:$DATA'
-dosattrib_b64='CTB4MTAAAAMAAwAAABEAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABimX3sSqfTAQAAAAAAAAAACg=='
-owner="root"
+dosattrib_b64="CTB4MTAAAAMAAwAAABEAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABimX3sSqfTAQAAAAAAAAAACg=="
+owner="root" 
 group="wheel"
 
 file_iteration=1
@@ -27,11 +27,11 @@ echo "$rnd_dir"
             rnd_file=$(mktemp "$rnd_dir"/smbtest.XXXXXXXX)
             dd_count=$(jot -r 1 $min_count $max_count)
             dd if=/dev/zero count=$dd_count bs=$dd_bs status=none | \
-                openssl enc -aes-256-ecb -k abcd123456 | dd of=$rnd_file
+                openssl enc -aes-256-ecb -k abcd123456 | dd of=$rnd_file status=none
             dd if=/dev/zero count=$dd_count bs=$dd_bs status=none | \
                 openssl enc -aes-256-ecb -k abcd123456 | \
                 setextattr -in "user" "$stream_name" $rnd_file
-            echo "$dosattrib_b64" | b64decode -r | \ 
+            echo -n "$dosattrib_b64" | b64decode -r | \
                 setextattr -i "user" "DOSATTRIB" $rnd_file
             file_iteration=$(($file_iteration + 1))
         done
