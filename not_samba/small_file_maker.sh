90c3ca7dc8042984cbadc935c27be664ef9c80ef
Andrew <walker.aj325@gmail.com>
2018-02-20 14:13:13 -0500

    Remove leading tab from dos stream

--- not_samba/small_file_maker.sh
+++ not_samba/small_file_maker.sh
@@ -3,15 +3,15 @@
 # Script to create lots of small files with xattrs for samba testing
 # Depends on openssl and winacl
 
-target_num_files=1000
-target_num_dirs=100
+target_num_files=100
+target_num_dirs=1
 target_dir="/mnt/flashyflashflash/SSD_SHARE/smb_small_files2"
 min_count=200  #minimum value for blocks in dd
 max_count=700  #maximum value for blocks in dd
 dd_bs=16384  #16KB
 stream_name='DosStream.Afp_Resource:$DATA'
-dosattrib_b64="CTB4MTAAAAMAAwAAABEAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABimX3sSqfTAQAAAAAAAAAACg=="
-owner="root" 
+dosattrib_b64="MHgxMAAAAwADAAAAEQAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKZfexKp9MBAAAAAAAAAAAK=="
+owner="root"
 group="wheel"
 
 file_iteration=1
@@ -26,10 +26,8 @@ echo "$rnd_dir"
         do
             rnd_file=$(mktemp "$rnd_dir"/smbtest.XXXXXXXX)
             dd_count=$(jot -r 1 $min_count $max_count)
-            dd if=/dev/zero count=$dd_count bs=$dd_bs status=none | \
-                openssl enc -aes-256-ecb -k abcd123456 | dd of=$rnd_file status=none
-            dd if=/dev/zero count=$dd_count bs=$dd_bs status=none | \
-                openssl enc -aes-256-ecb -k abcd123456 | \
+            dd if=/dev/random count=$dd_count bs=$dd_bs of=$rnd_file status=none
+            dd if=/dev/random count=$dd_count bs=$dd_bs status=none | \
                 setextattr -in "user" "$stream_name" $rnd_file
             echo -n "$dosattrib_b64" | b64decode -r | \
                 setextattr -i "user" "DOSATTRIB" $rnd_file
