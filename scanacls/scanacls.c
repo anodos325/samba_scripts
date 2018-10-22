f180a8ffcd3b995bceb48387daf8a384c0ff97bf
Andrew <awalker@ixsystems.com>
2018-10-22 17:26:53 -0400

    trim the fat

--- scanacls/scanacls.c
+++ scanacls/scanacls.c
@@ -26,122 +26,53 @@ usage(void)
         fprintf(stderr, "scanacls [-xrv] [file ...]\n");
 }
 
-static void
-setarg(char **pptr, const char *src)
-{
-	char *ptr;
-
-	ptr = *pptr;
-	if (ptr != NULL)
-		free(ptr);
-	ptr = strdup(src);
-	if (ptr == NULL)
-		err(EX_OSERR, NULL);
-
-	*pptr = ptr;
-}
-
-struct file_info {
-	char *path;
-	char *real_path;
-	struct stat st;
-	
-};
-
-static struct file_info *
-new_file_info(void)
-{
-	struct file_info *s;
-
-	if ((s = malloc(sizeof(*s))) == NULL)
-		err(EX_OSERR, "malloc() failed");
-
-	s->path = NULL;
-	s->real_path = NULL;
-	return (s);
-}
-
-static void
-free_file_info(struct file_info *f)
-{
-	if (f == NULL)
-		return;
-
-	free(f->path);
-	free(f->real_path);
-	free(f);
-}
-
 int
-get_acl_type(struct file_info *file_info)
+get_acl_type(FTSENT *fts_entry)
 {
 	int acl_type;
 
-	if ((file_info->st.st_mode |= S_IFLNK) == 1) {
-		acl_type = lpathconf(file_info->path, _PC_ACL_NFS4) == 1 ? ACL_TYPE_NFS4 : ACL_TYPE_ACCESS;
+	if ((fts_entry->fts_statp->st_mode |= S_IFLNK) == 1) {
+		acl_type = lpathconf(fts_entry->fts_accpath, _PC_ACL_NFS4) == 1 ? ACL_TYPE_NFS4 : ACL_TYPE_ACCESS;
 	}
 	else {
-		acl_type = pathconf(file_info->path, _PC_ACL_NFS4) == 1 ? ACL_TYPE_NFS4 : ACL_TYPE_ACCESS;
+		acl_type = pathconf(fts_entry->fts_accpath, _PC_ACL_NFS4) == 1 ? ACL_TYPE_NFS4 : ACL_TYPE_ACCESS;
 	}
 
 	return acl_type;
 }
 
-int
-get_stat_info(struct file_info *file_info)
-{
-	int ret;
-	/* handling for broken symlinks */
-
-	if ((ret = stat(file_info->path, &file_info->st)) == -1 &&
-		errno == ENOENT &&
-		(ret = lstat(file_info->path, &file_info->st)) == -1) {
-			errno = ENOENT;
-			return -1;	
-	}
-
-	return ret;
-}
-
 int                             
-print_output(struct file_info *file_info)
+print_output(FTSENT *fts_entry)
 {
+	char buf[PATH_MAX + 1];
+	char *real_path =  realpath(fts_entry->fts_accpath, buf);
 	xo_open_container("path");
-	xo_emit("{:Realpath/%s} |",file_info->real_path);
+	xo_emit("{:Realpath/%s} |",real_path);
 	xo_close_container("path");
 
 	xo_open_container("stat");
 	xo_emit("{:is_dir/%d}| {:atime/%d} {:mtime/%d} {:ctime/%d} {:btime/%d}\n",
-		 (file_info->st.st_mode & S_IFDIR ? 1 : 0),
-		 file_info->st.st_atim, file_info->st.st_mtim,
-		 file_info->st.st_ctim, file_info->st.st_birthtim);
+		 (fts_entry->fts_statp->st_mode & S_IFDIR ? 1 : 0),
+		 fts_entry->fts_statp->st_atim, fts_entry->fts_statp->st_mtim,
+		 fts_entry->fts_statp->st_ctim, fts_entry->fts_statp->st_birthtim);
 	xo_close_container("stat");
-
+	free(real_path);
+	free(buf);
 	return 0;
 }
 
 static int
-scan_acl(char *path)
+scan_acl(FTSENT *fts_entry)
 {
 	int ret;
-	char buf[PATH_MAX + 1];
 	acl_t tmp_acl;
-	struct file_info *file_info;
-	file_info = new_file_info();
-
-	setarg(&file_info->path, path);
-	get_stat_info(file_info);
-	ret = get_acl_type(file_info);
+	ret = get_acl_type(fts_entry);
 	if (ret == ACL_TYPE_NFS4) {
-		if ((tmp_acl = acl_get_file(file_info->path, ACL_TYPE_NFS4)) == NULL) { 
-			char *real_path =  realpath(path, buf);
-			setarg(&file_info->real_path, real_path);
-			get_stat_info(file_info);
-			print_output(file_info);	
+		if ((tmp_acl = acl_get_file(fts_entry->fts_accpath, ACL_TYPE_NFS4)) == NULL) { 
+			print_output(fts_entry);	
 		}
 		acl_free(tmp_acl);
 	}  
-	free_file_info(file_info);
 }
 
 static int
@@ -159,15 +90,9 @@ scan_acls(char *path, int flags)
 	char *paths[4];
 	int rval;
 
-	/* recursive not set, only do this entry */
-	if (!(flags & FLAG_RECURSIVE)) {
-		scan_acl(path);
-		return (0);
-	}
-
 	paths[0] = path;
 	paths[1] = NULL;
-	options = FTS_LOGICAL|FTS_NOSTAT;
+	options = FTS_LOGICAL;
 
 	if ((tree = fts_open(paths, options, fts_compare)) == NULL)
 		err(EX_OSERR, "fts_open");
@@ -177,7 +102,7 @@ scan_acls(char *path, int flags)
 		switch (entry->fts_info) {
 			case FTS_D:
 			case FTS_F:
-				scan_acl(entry->fts_accpath);
+				scan_acl(entry);
 				break;	
 
 			case FTS_ERR:
